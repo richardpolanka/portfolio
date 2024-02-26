6e21bc574164e56cd56158ed7532c9c26dffd5a6
Richard Polanka <richard.polanka@gmail.com>
2024-02-26 12:39:22 +0100

    First commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-      },
-    distDir: 'build',
 };
 
 module.exports = nextConfig;
